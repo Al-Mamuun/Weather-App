443982ca66cffb7f318d6e6860fa4089e662c3e2
Mamuun <almamun19403@gmail.com>
2026-09-23 15:54:57 +0600

    Modal Management

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,32 @@
 import { useState } from "react";
+import LocationModal from "../components/LocationModal";
 
 
 function Home() {
 
-  const [click, setClick] = useState("");
+  const [click, setClick] = useState(false);
   console.log(click);
   return (
-    <div className="text-center">
-      <h1 className="text-6xl text-blue-300 font-extrabold">
-        NextLevel <span className="text-blue-400">Weather</span>
-      </h1>
-      <p className="py-4 text-md text-gray-400">
-        Check your weather today in next level.
-      </p>
+    <div>
+      <div className="text-center">
+        <h1 className="text-6xl text-blue-300 font-extrabold">
+          NextLevel <span className="text-blue-400">Weather</span>
+        </h1>
+        <p className="py-4 text-md text-gray-400">
+          Check your weather today in next level.
+        </p>
+      </div>
 
-      <div>
+      <div className="text-center">
         <button
           type="button"
           onClick={() => setClick("Clicked")}
-          className="text-lg font font-medium hover:scale-105 transition-all delay-500 bg-blue-500 px-5 py-1 rounded-4xl text-gray-100"
+          className="text-lg font-medium hover:scale-105 transition-all delay-70 bg-blue-500 px-5 py-1 rounded-4xl text-gray-100"
         >
           Check Weather
         </button>
       </div>
+      {click && <LocationModal onClose={() => setClick(false)} />}
     </div>
   );
 }
